658ce4920a0cc9b9bd27dcd18ee98928f68fec42
Gregor Berginc <gregor.berginc@xlab.si>
2017-03-29 11:30:10 +0200

    Enhance file upload script
    Make the script for uploading photos a bit more customisable, expecting
    the URL of the key-value store service and path to the phoot user is
    uploading.

--- bin/upload_batch.sh
+++ bin/upload_batch.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-MASTER_ENDPOINT=`curl -s http://192.168.122.231:8000/masterendpoint`
+# Usage: 
+#
+# ./bin/upload_batch.sh KEYVALUE_ENDPOINT IMAGE_PATH N_REPETITIONS
+#
+
+MASTER_ENDPOINT=`curl -s http://$1/masterendpoint`
 
 echo "Uploading to $MASTER_ENDPOINT"
 
-for i in $(seq 1 $1)
+for i in $(seq 1 $3)
 do 
-	curl -X POST -F "image=@/home/lemmy/dl/IMG_0995.PNG" http://$MASTER_ENDPOINT/task
+	curl -X POST -F "image=@$2" http://$MASTER_ENDPOINT/task
 done
